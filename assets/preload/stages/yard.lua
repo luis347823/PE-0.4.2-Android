9396e18c664294094e326d57704e9c7a0d7f59f0
luis347823 <102200261+luis347823@users.noreply.github.com>
2022-03-22 21:47:08 -0500

    Add files via upload

---
+++ assets/preload/stages/yard.lua
@@ -0,0 +1,8 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('sky', 'sky', -600, -300);
+	setScrollFactor('sky', 0.9, 0.9);
+	
+	makeLuaSprite('yard', 'yard', -650, 600);
+	setScrollFactor('yard', 0.9, 0.9);
+	scaleObject('yard', 1.1, 1.1);
